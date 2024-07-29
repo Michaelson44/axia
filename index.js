9d5b740fbd0f226eda87c952abf7ac2563077e44
Michaelson44 <149709538+Michaelson44@users.noreply.github.com>
2024-07-29 12:25:18 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// Part two of axia backend exam
+
+const myStudent = ["michaelson1"];
+
+for(let i = 0; i <= myStudent.length; i++) {
+    console.log(myStudent[i]);
+}
+
+// CREATING AND LOGGING OBJECT WITH PROPERTIES
+
+const person = {
+    name: "michael",
+    age: 23,
+    married: false,
+    location: null
+}
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.married);
+console.log(person.location);
+
